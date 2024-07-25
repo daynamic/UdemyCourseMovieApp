abffe7f9c9cb527034eda4f349b3448ce4d01b49
Akshat <akshat@Akshats-MacBook-Pro.local>
2024-07-25 13:54:29 -0400

    till basic Lazycolumn view in data

--- app/src/main/java/com/akshat/udemycoursemovieapp/MainActivity.kt
+++ app/src/main/java/com/akshat/udemycoursemovieapp/MainActivity.kt
@@ -1,47 +1,215 @@
 package com.akshat.udemycoursemovieapp
 
+import android.health.connect.datatypes.units.Length
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.akshat.udemycoursemovieapp.ui.theme.PurpleGrey40
 import com.akshat.udemycoursemovieapp.ui.theme.UdemyCourseMovieAppTheme
+import kotlinx.coroutines.withContext
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            UdemyCourseMovieAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            MyApp {
+                MainContent()
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun MyApp(content: @Composable () -> Unit) {
+    UdemyCourseMovieAppTheme {
+        Scaffold(modifier = Modifier,
+            topBar = {
+                TopAppBar(
+                    title = {
+                        Text("Moviess")
+                    },
+                    colors = TopAppBarDefaults.smallTopAppBarColors(
+                        containerColor = Color.Magenta
+                    )
+                )
+            }
+        ) { it ->
+            Surface(modifier = Modifier.padding(top = it.calculateTopPadding())) {
+                content()
+            }
+        }
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AddAppBar() {
+    TopAppBar(
+        title = {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    modifier = Modifier
+                        .size(48.dp)
+                        .fillMaxWidth()
+                        .padding(end = 8.dp),
+                    imageVector = Icons.Filled.AccountCircle,
+                    contentDescription = "profile picture",
+                )
+                Text("Movies")
+            }
+        },
+        navigationIcon = {
+            IconButton({}) {
+                Icon(
+                    imageVector = Icons.Filled.ArrowBack,
+                    contentDescription = "menu items"
+                )
+            }
+        },
+        actions = {
+            IconButton(onClick = {}) {
+                Icon(
+                    imageVector = Icons.Filled.Add,
+                    contentDescription = "video call",
+                )
+            }
+            IconButton(onClick = {}) {
+                Icon(
+                    imageVector = Icons.Filled.Call,
+                    contentDescription = "phone call",
+                )
+            }
+            IconButton(onClick = {}) {
+                Icon(
+                    imageVector = Icons.Filled.MoreVert,
+                    contentDescription = "more options",
+                )
+            }
+        },
+        colors = TopAppBarDefaults.topAppBarColors(MaterialTheme.colorScheme.primary),
     )
 }
+
+@Composable
+fun MainContent(
+    movieList: List<String> = listOf(
+        "Avatar",
+        "300",
+        "Harry Potter",
+        "Life",
+        "Love",
+        "Hate",
+        "Avatar",
+        "300",
+        "Harry Potter"
+    )
+) {
+    Column(modifier = Modifier.padding(12.dp)) {
+        val context = LocalContext.current
+        LazyColumn {
+            items(items = movieList) {
+                MovieRow(movie = it){movie ->
+                    Toast.makeText(context, "Clikced on $movie", Toast.LENGTH_SHORT).show()
+
+                }
+            }
+        }
+
+    }
+
+
+}
+
+@Composable
+fun MovieRow(movie: String, onItemClick: (String) -> Unit) {
+    Card(
+        modifier = Modifier
+            .padding(4.dp)
+            .fillMaxWidth()
+            .height(130.dp)
+            .clickable{
+                onItemClick(movie)
+            },
+        shape = RoundedCornerShape(corner = CornerSize(16.dp)),
+        elevation = CardDefaults.cardElevation(6.dp)
+    )
+    {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Surface(
+                modifier = Modifier
+                    .padding(12.dp)
+                    .size(100.dp),
+                shape = RectangleShape,
+                tonalElevation = 4.dp
+            ) {
+                Icon(
+                    imageVector = Icons.Default.AccountBox,
+                    contentDescription = "Movie Image"
+                )
+
+            }
+            Text(text = movie)
+        }
+
+    }
+
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    UdemyCourseMovieAppTheme {
-        Greeting("Android")
+    MyApp {
+        MainContent()
     }
 }
